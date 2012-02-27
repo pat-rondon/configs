cca287e1eae1250298d3a5d154478bdd1c1aece1
Pat Rondon <prondon@cs.ucsd.edu>
2012-02-27 14:02:43 -0800

    drop some latex config

--- emacs/latex-pmr.el
+++ emacs/latex-pmr.el
@@ -1,9 +1,3 @@
-(require 'tex-site)
 (setq tex-dvi-view-command "xdvi")
 (setq-default TeX-master nil)
 (setq-default TeX-parse-self t)
-
-(defun set-TeX-faces ()
-  (color-theme-froufrou))
-
-(add-hook 'TeX-mode-hook 'set-TeX-faces)
