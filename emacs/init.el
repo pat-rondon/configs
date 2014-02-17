32e6c760869b05de368f2775c6c204175f255170
Pat Rondon <prondon@cs.ucsd.edu>
2014-02-17 18:45:29 -0500

    Remove MELPA from config

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,4 @@
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
-                         ("MELPA" . "http://melpa.milkbox.net/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 
